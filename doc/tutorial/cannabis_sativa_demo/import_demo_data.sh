7fe5e86b462dc5d5365eebd7594b815983fad9f3
Frank Förster <foersterfrank@gmx.de>
2014-11-20 10:39:35 +0100

    import_demo_data.sh: Generating new database schema for Worker DB

--- doc/tutorial/cannabis_sativa_demo/import_demo_data.sh
+++ doc/tutorial/cannabis_sativa_demo/import_demo_data.sh
@@ -72,6 +72,85 @@ tbro-import differential_expressions -o $ORGANISM -r 1.CasaPuKu --analysis_id $A
 MD5SUM_TRANSCRIPTS=$(md5sum blast/cannabis_sativa_transcriptome.zip | cut -f 1 -d " ")
 MD5SUM_PROTEINS=$(md5sum blast/cannabis_sativa_predpep.zip | cut -f 1 -d " ")
 
+cat > /tmp/input.sql <<EOF
+BEGIN TRANSACTION;
+
+-- programs that the user is allowed to execute
+INSERT INTO programs (name) VALUES
+('blastn'),
+('blastp'),
+('blastx'),
+('tblastn'),
+('tblastx');
+
+-- database files available. name is the name it will be referenced by, md5 is the zip file's sum, download_uri specifies where the file can be retreived
+INSERT INTO database_files
+ (name, md5, download_uri) VALUES
+ ('cannabis_sativa_transcriptome.fasta', '${MD5SUM_TRANSCRIPTS}',
+ 'ftp://WORKER/cannabis_sativa_transcriptome.zip'),
+ ('cannabis_sativa_predpep.fasta', '${MD5SUM_PROTEINS}',
+ 'ftp://WORKER/cannabis_sativa_predpep.zip');
+
+-- contains information which program is available for which program.
+-- additionally, 'availability_filter' can be used to e.g. restrict use for a organism-release combination
+INSERT INTO program_database_relationships
+ (programname, database_name, availability_filter) VALUES
+ ('blastn','cannabis_sativa_transcriptome.fasta', '${ORGANISM}_1.CasaPuKu'),
+ ('blastp','cannabis_sativa_predpep.fasta', '${ORGANISM}_1.CasaPuKu'),
+ ('blastx','cannabis_sativa_predpep.fasta', '${ORGANISM}_1.CasaPuKu'),
+ ('tblastn','cannabis_sativa_transcriptome.fasta', '${ORGANISM}_1.CasaPuKu'),
+ ('tblastx','cannabis_sativa_transcriptome.fasta', '${ORGANISM}_1.CasaPuKu');
+
+--time in seconds until a query job will be set from "PROCESSING" to "NOT_PROCESSED". make sure this value is big enough or some jobs will stay in the queue forever.
+UPDATE options SET value=120 WHERE key='MAXIMUM_EXECUTION_TIME';
+--time in seconds a worker has to send another keepalive_ping until a query job will be set from "PROCESSING" to "NOT_PROCESSED".
+UPDATE options SET value=15 WHERE key='MAXIMUM_KEEPALIVE_TIMEOUT';
+
+
+-- allowed parameters to be passed for each of these programs
+-- if a parameter is omited by create_job, the default_value will be used. can contain the variable $DBFILE
+-- the constraint_function will be executed on the parameter value, together with constraint_function_parameters
+-- available constraints function are (but can be extended):
+--   cfunc_in_array - takes an array of values as constraint_function_parameters
+--   cfunc_within_bounds - takes an array of {min,max} as constraint_function_parameters
+--   cfunc_default_only - user can not change it, always use default parameter
+INSERT INTO allowed_parameters
+(programname, param_name, default_value, constraint_function, constraint_function_parameters) VALUES
+('blastn',  'task',             'megablast', 'cfunc_in_array',      ARRAY['blastn', 'dc-megablast', 'megablast']),
+('blastn',  'word_size',        '11',        'cfunc_in_array',      ARRAY['7', '11', '15']),
+('blastn',  'outfmt',           '5',         'cfunc_default_only',  NULL),
+('blastn',  'max_target_seqs',  '10',        'cfunc_within_bounds', ARRAY['1','1000']),
+('blastn',  'evalue',           '0.1',       'cfunc_within_bounds', ARRAY['0','100']),
+('blastn',  'db',               '$DBFILE',   'cfunc_default_only',  NULL),
+('blastp',  'task',             'blastp',    'cfunc_default_only',  NULL),
+('blastp',  'word_size',        '3',         'cfunc_in_array',      ARRAY['2', '3']),
+('blastp',  'outfmt',           '5',         'cfunc_default_only',  NULL),
+('blastp',  'max_target_seqs',  '10',        'cfunc_within_bounds', ARRAY['1','1000']),
+('blastp',  'evalue',           '0.1',       'cfunc_within_bounds', ARRAY['0','100']),
+('blastp',  'matrix',           'BLOSUM62',  'cfunc_in_array',      ARRAY['BLOSUM45', 'BLOSUM50', 'BLOSUM62', 'BLOSUM80', 'BLOSUM90', 'PAM30', 'PAM70', 'PAM250']),
+('blastp',  'db',               '$DBFILE',   'cfunc_default_only',  NULL),
+('blastx',  'outfmt',           '5',         'cfunc_default_only',  NULL),
+('blastx',  'word_size',        '3',         'cfunc_in_array',      ARRAY['2', '3']),
+('blastx',  'max_target_seqs',  '10',        'cfunc_within_bounds', ARRAY['1','1000']),
+('blastx',  'evalue',           '0.1',       'cfunc_within_bounds', ARRAY['0','100']),
+('blastx',  'matrix',           'BLOSUM62',  'cfunc_in_array',      ARRAY['BLOSUM45', 'BLOSUM50', 'BLOSUM62', 'BLOSUM80', 'BLOSUM90', 'PAM30', 'PAM70', 'PAM250']),
+('blastx',  'db',               '$DBFILE',   'cfunc_default_only',  NULL),
+('tblastn', 'outfmt',           '5',         'cfunc_default_only',  NULL),
+('tblastn', 'word_size',        '3',         'cfunc_in_array',      ARRAY['2', '3']),
+('tblastn', 'max_target_seqs',  '10',        'cfunc_within_bounds', ARRAY['1','1000']),
+('tblastn', 'evalue',           '0.1',       'cfunc_within_bounds', ARRAY['0','100']),
+('tblastn', 'matrix',           'BLOSUM62',  'cfunc_in_array',      ARRAY['BLOSUM45', 'BLOSUM50', 'BLOSUM62', 'BLOSUM80', 'BLOSUM90', 'PAM30', 'PAM70', 'PAM250']),
+('tblastn', 'db',               '$DBFILE',   'cfunc_default_only',  NULL),
+('tblastx', 'outfmt',           '5',         'cfunc_default_only',  NULL),
+('tblastx', 'word_size',        '3',         'cfunc_in_array',      ARRAY['2', '3']),
+('tblastx', 'max_target_seqs',  '10',        'cfunc_within_bounds', ARRAY['1','1000']),
+('tblastx', 'evalue',           '0.1',       'cfunc_within_bounds', ARRAY['0','100']),
+('tblastx', 'matrix',           'BLOSUM62',  'cfunc_in_array',      ARRAY['BLOSUM45', 'BLOSUM50', 'BLOSUM62', 'BLOSUM80', 'BLOSUM90', 'PAM30', 'PAM70', 'PAM250']),
+('tblastx', 'db',               '$DBFILE',   'cfunc_default_only',  NULL);
+
+COMMIT;
+EOF
+
 ### Call this manually with a valid feature_id
 # FEATUREID=
 # tbro-db feature add_synonym -f $FEATUREID --synonym 'InterestingTranscript' -b '[[publication/1ec511cfe178d54ff2ce82043ae958538/iimog]]' -u 'tbro' -t symbol -k 79595d0d12b7667344dd6d326fc7ba42
