4a9846a1f39e1c532a5cde35d5b2f65b3a1c8c57
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2014-11-20 18:43:50 +0100

    Added a section to set 100 random synonym names in demo data

--- doc/tutorial/cannabis_sativa_demo/import_demo_data.sh
+++ doc/tutorial/cannabis_sativa_demo/import_demo_data.sh
@@ -162,10 +162,13 @@ psql --host=WORKER --port=$WORKER_PORT_5432_TCP_PORT --username=$WORKER_ENV_DB_U
 # finally we have to upload the files to our FTP container
 curl --progress-bar --data-binary --ftp-pasv --user "$WORKERFTP_ENV_FTP_USER":"$WORKERFTP_ENV_FTP_PW" -T blast/cannabis_sativa_transcriptome.zip ftp://WORKERFTP/
 curl --progress-bar --data-binary --ftp-pasv --user "$WORKERFTP_ENV_FTP_USER":"$WORKERFTP_ENV_FTP_PW" -T blast/cannabis_sativa_predpep.zip ftp://WORKERFTP/
-
-### Call this manually with a valid feature_id
-# FEATUREID=
-# tbro-db feature add_synonym -f $FEATUREID --synonym 'InterestingTranscript' -b '[[publication/1ec511cfe178d54ff2ce82043ae958538/iimog]]' -u 'tbro' -t symbol -k 79595d0d12b7667344dd6d326fc7ba42
+for j in $(for i in $(sort --random-sort transcriptome/cannabis_sativa_transcriptome.ids | head -100); do tbro-db feature list --name "$i" | sed -n '4{p;q}' | cut -f 2 -d "|"; done)
+do 
+    FEATUREID=$j
+    tbro-db feature add_synonym -f $FEATUREID --synonym 'InterestingTranscript'_$j -b '[[publication/1ec511cfe178d54ff2ce82043ae958538/iimog]]' -u 'tbro' -t symbol -k 79595d0d12b7667344dd6d326fc7ba42 
+done
+FEATUREID=$(tbro-db feature list --name $(head -n1 transcriptome/cannabis_sativa_transcriptome.ids) | sed -n '4{p;q}' | cut -f 2 -d "|")
+tbro-db feature add_synonym -f $FEATUREID --synonym 'InterestingTranscript' -b '[[publication/1ec511cfe178d54ff2ce82043ae958538/iimog]]' -u 'tbro' -t symbol -k 79595d0d12b7667344dd6d326fc7ba42
 
 tbro-tools addECInformationToDB ec2kegg/ec_info.tab
 tbro-tools addPathwayInformationToDB ec2kegg/kegg_info.tab
